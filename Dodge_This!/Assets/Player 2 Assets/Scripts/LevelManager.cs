da023c379c59280498f82c7b78e15b952e1bf4c9
KaktusPai <59810116+KaktusPai@users.noreply.github.com>
2021-10-19 19:53:46 +0200

    Made tempscene, jump and levelmanager

---
+++ Dodge_This!/Assets/Player 2 Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject spawnable;
+    ObjectSpawning objectSpawning;
+    public bool levelActive = false;
+    void Start()
+    {
+        objectSpawning = new ObjectSpawning(spawnable);
+        objectSpawning.localTrigger = levelActive;
+    }
+
+    void Update()
+    {
+        objectSpawning.Tick();
+    }
+}
---
+++ Dodge_This!/Assets/Player 2 Assets/Scripts/ObjectSpawning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawning : MonoBehaviour
+{
+    // TEMP SPAWNING FOR TESTING
+    public GameObject shootShape;
+    public bool localTrigger = false;
+    public ObjectSpawning(GameObject _shootShape) //empty construct
+    {
+        shootShape = _shootShape;
+    }
+    public void Tick()
+    {
+        // SPAWN FOR TESTING 
+        if (localTrigger)
+        { 
+            { 
+                StartCoroutine(SpawnObjectOnRythm());
+                localTrigger = false;
+            }
+        }
+    }
+    IEnumerator SpawnObjectOnRythm()
+    { 
+        yield return new WaitForSeconds(1);
+        print("kaas");
+        localTrigger = true;
+    }
+}
